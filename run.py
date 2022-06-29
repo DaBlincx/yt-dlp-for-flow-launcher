ce6396c1faf0eaed3e52f35a94906e928c23febc
DaBlincx <blincxyt@gmail.com>
2022-06-29 21:36:10 +0200

    main commit i think, nothing rly has happened yet

--- run.py
+++ run.py
@@ -4,3 +4,6 @@ parent_folder_path = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(parent_folder_path)
 sys.path.append(os.path.join(parent_folder_path, 'lib'))
 sys.path.append(os.path.join(parent_folder_path, 'plugin'))
+
+if __name__ == '__main__':
+    
